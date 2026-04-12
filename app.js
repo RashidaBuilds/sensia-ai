b6d0a2090f46bf6c867dde17c3c5157f680228e6
Rashida Abdul-rashid <shidak360@yahoo.com>
2026-04-12 18:04:17 +0100

    Sensia AI Basic UI
    Basic UI for Sensia AI

---
+++ app.js
@@ -0,0 +1,27 @@
+const input = document.getElementById("userInput");
+const button = document.getElementById("sendBtn");
+const message = document.getElementById("assistantMessage");
+const orb = document.getElementById("orb");
+
+button.addEventListener("click", handleSearch);
+
+function handleSearch() {
+  const text = input.value;
+  if (!text) return;
+
+  orb.classList.add("active");
+
+  message.innerText = "Thinking...";
+
+  setTimeout(() => {
+    message.innerText = "Tracing places from your memory...";
+
+    setTimeout(() => {
+      message.innerText = "I found a few places that match";
+
+      orb.classList.remove("active");
+    }, 2000);
+  }, 1000);
+
+  input.value = "";
+}
